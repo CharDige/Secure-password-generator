cd783947a548a6dff3df40932f8dab952bf7e37f
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-03-26 14:04:52 +1030

    Added number of characters requirement into generatePassword function

--- script.js
+++ script.js
@@ -10,14 +10,33 @@ function writePassword() {
 
 }
 
+// Password requirement variables
 var uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowers = "abcdefghijklmnopqrstuvwxyz";
 var nums = "0123456789";
 var specialChars = "!#$%&'()*+,-./:;<=>?@[]^_`{}|~"
 
-
+// generatePassword function
 function generatePassword() {
+    // Ask for number of characters
+    var numChars = window.prompt("Enter number of characters for password length");
 
+    // If user a user selects a number below 8, an alert pops up that password length must be longer
+    if (numChars < 8) {
+        window.alert("Password must be at least 8 characters");
+        generatePassword();
+    
+    // If user selects a number greater than 128, an alert pops up that the password length must be shorter
+    } else if (numChars > 128) {
+        window.alert("Password must not exceed 128 characters");
+        generatePassword();
+    
+    // If user selects a number between 8 and 128, then it saves response for length of password to generate
+    } else {
+        var passwordLength = numChars;
+    }
+
+    
 }
 
 // Add event listener to generate button
